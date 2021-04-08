4c5c6a30a0feb872cde80659536eddd6a95a2ad5
wuyan <38306749+YZY418@users.noreply.github.com>
2021-04-08 23:08:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
+git clone https://github.com/kenzok8/small.git  package/openwrt-small
